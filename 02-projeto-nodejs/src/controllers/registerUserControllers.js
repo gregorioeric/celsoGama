25f674eefd4d4f2da910f95b667b82c99ebfc37d
Eric Gregorio <gregorio.eric@gmail.com>
2024-05-23 11:52:32 -0300

    Modificado os arquivos register controller e register ejs

--- 02-projeto-nodejs/src/controllers/registerUserControllers.js
+++ 02-projeto-nodejs/src/controllers/registerUserControllers.js
@@ -3,18 +3,21 @@ class RegisterUserControllers {
     return res.render("register", {
       msgFieldsEmpty: "",
       msgNameError: "",
+      msgPassError: "",
+      msgEmailError: "",
     });
   }
 
   store(req, res) {
     const { ...data_user } = req.body;
 
-    if (data_user.user_name < 3) {
-      return res.render("register", {
-        msgNameError: "Nome precisa ter no minino 3 caracteres!",
-        msgFieldsEmpty: "",
-      });
-    }
+    const regex_email = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const regex_senha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$#@%*&!?])/;
+
+    const regex_maiuscula = /^(?=.*[A-Z])/;
+    const regex_minuscula = /^(?=.*[a-z])/;
+    const regex_numero = /^(?=.*[0-9])/;
+    const regex_caracter_especial = /^(?=.*[$#@%*&!?])/;
 
     if (
       !data_user.user_name ||
@@ -25,11 +28,36 @@ class RegisterUserControllers {
       return res.render("register", {
         msgFieldsEmpty: "Os campos não podem ser vazios!",
         msgNameError: "",
+        msgPassError: "",
+        msgEmailError: "",
+      });
+    }
+
+    if (data_user.user_name.length < 3) {
+      return res.render("register", {
+        msgNameError: "Nome precisa ter no minino 3 caracteres!",
+        msgFieldsEmpty: "",
+        msgPassError: "",
+        msgEmailError: "",
+      });
+    }
+
+    if (!regex_email.test(data_user.user_email)) {
+      return res.render("register", {
+        msgEmailError: "Digite um email valido!",
+        msgNameError: "",
+        msgFieldsEmpty: "",
+        msgPassError: "",
       });
     }
 
     if (data_user.user_password !== data_user.userConfirmPassword) {
-      return res.send("Senhas não são iguais!");
+      return res.render("register", {
+        msgPassError: "Senha não são iguais",
+        msgNameError: "",
+        msgFieldsEmpty: "",
+        msgEmailError: "",
+      });
     }
 
     console.log(data_user);
